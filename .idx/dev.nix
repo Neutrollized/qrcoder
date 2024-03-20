69c4e075bbe796a1cd9cee29bb5712d209ed3d59
glen.yu <glen.yu@gmail.com>
2024-03-20 04:26:05 +0000

    reorganized files, added idx config

---
+++ .idx/dev.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python311
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    QRCODE_APP_GCS = "qrcode-test";
+    PORT = "80";
+  };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+      "ms-python.python"
+      "vscodevim.vim"
+  ];
+
+  idx.workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onStart = {
+        install =
+          "python -m venv .venv  && source .venv/bin/activate  &&  pip install -r requirements.txt";
+      };
+      # To run something each time the environment is rebuilt, use the `onStart` hook
+  };
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+    previews = [
+      {
+        command = [ "./devserver.sh" ];
+        env = { PORT = "$PORT"; };
+        manager = "web";
+        id = "web";
+      }
+    ];
+  };
+}
